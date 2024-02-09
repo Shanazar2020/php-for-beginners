1c76e9808e41e49a3f5297213ca181595d28d4b3
Shanazar <snizamaddinow@gmail.com>
2024-02-10 00:06:45 +0500

    functions

--- app/index.php
+++ app/index.php
@@ -16,27 +16,51 @@
 <body>
     <?php
         $books = [
-                'Do Androids Dream of Electric Sheep?',
-            'Animal Farm',
-            'Us',
-            "Tha Langoliers",
-        ]
+            [
+                'name' => 'Do Androids Dream of Electric Sheep',
+                'author' => 'Philip K. Dick',
+                'releaseYear' => 1968,
+                'purchaseUrl' => 'https://example.com',
+            ],
+            [
+                'name' => 'Project Hail Mary',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2022,
+                'purchaseUrl' => 'https://example.com'
+            ],
+            [
+                'name' => 'The Martian',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2011,
+                'purchaseUrl' => 'https://example.com'
+            ],
+        ];
+
+        function filterByAuthor($books, $author){
+            $filteredBooks = [];
+
+            foreach ($books as $book){
+                if ($book['author'] === $author){
+                    $filteredBooks[] = $book;
+                }
+            }
+
+            return $filteredBooks;
+        }
+
     ?>
 
     <h1>
         Recommended Books
     </h1>
     <ul>
-        <?php foreach ($books as $book) : ?>
-            <li> <?= $book ?></li>
+        <?php foreach(filterByAuthor($books, 'Andy Weir') as $book): ?>
+            <li>
+                <a href="<?= $book['purchaseUrl'] ?>">
+                    <?= $book['name'] ?> - <?= $book['author'] ?> - <?= $book['releaseYear'] ?>
+                </a>
+            </li>
         <?php endforeach; ?>
-
-
-        <?php
-            foreach ($books as $book) {
-                echo "<li>{$book}</li>";
-            }
-        ?>
     </ul>
 </body>
 </html>
