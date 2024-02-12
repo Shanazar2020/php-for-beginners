fb2b199d3665ea66273685104505b8c6e4c758b8
Shanazar <snizamaddinow@gmail.com>
2024-02-12 18:10:27 +0500

    routes optimized

--- app/functions.php
+++ app/functions.php
@@ -1,5 +1,18 @@
 <?php
 
+function dd($var)
+{
+    echo "<pre>";
+    var_dump($var);
+    echo "</pre>";
+
+    die();
+
+}
+
 function urlIs($uri){
     return $_SERVER['REQUEST_URI'] == $uri;
 }
+
+
+
---
+++ app/router.php
@@ -0,0 +1,30 @@
+<?php
+
+$path = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+    '/' => 'controllers/home.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+];
+
+function abort($code = 404)
+{
+    http_response_code($code);
+
+    require "views/{$code}.php";
+
+    die();
+}
+
+function routeToController($uri, $routes)
+{
+    if (array_key_exists($uri, $routes)){
+        require $routes[$uri];
+    } else {
+        abort();
+    }
+}
+
+routeToController($path, $routes);
+
