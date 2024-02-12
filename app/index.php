0e8fc9320aff153aad17bbf3a8d8937aed5764bb
Shanazar <snizamaddinow@gmail.com>
2024-02-12 14:56:45 +0500

    html skeleton

--- app/index.php
+++ app/index.php
@@ -1,29 +1,5 @@
 <?php
 
-$books = [
-    [
-        'name' => 'Do Androids Dream of Electric Sheep',
-        'author' => 'Philip K. Dick',
-        'releaseYear' => 1968,
-        'purchaseUrl' => 'https://example.com',
-    ],
-    [
-        'name' => 'Project Hail Mary',
-        'author' => 'Andy Weir',
-        'releaseYear' => 2022,
-        'purchaseUrl' => 'https://example.com'
-    ],
-    [
-        'name' => 'The Martian',
-        'author' => 'Andy Weir',
-        'releaseYear' => 2011,
-        'purchaseUrl' => 'https://example.com'
-    ],
-];
+$heading = "Home";
 
-
-$filteredBooks = array_filter($books, function ($book) {
-    return $book['releaseYear'] >= 1950 && $book['releaseYear'] <= 2020;
-});
-
-require "index.view.php";
+require "views/index.view.php";
